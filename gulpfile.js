ea4faadee60d3d4d890e060e222f368f23253a25
Nathan Conley <nateconley123@gmail.com>
2016-05-03 21:27:59 -0700

    Updated gulpfile to include css map

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,13 @@
 
 const gulp = require('gulp');
 const sass = require('gulp-sass');
+const maps = require('gulp-sourcemaps');
 
 gulp.task('sass', function(){
 	return gulp.src('dev/sass/styles.sass')
+		.pipe(maps.init())
 		.pipe(sass())
+		.pipe(maps.write('./'))
 		.pipe(gulp.dest('css/'))
 });
 
