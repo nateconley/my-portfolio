18449b23bc0f268891f3dfa6a2700dfe5dd1a38f
Nathan Conley <nateconley123@gmail.com>
2016-05-04 18:03:33 -0700

    Fine tuned things a bit

--- single-portfolio.php
+++ single-portfolio.php
@@ -5,13 +5,8 @@
 	<div class="single-content">
 
 		<h2 class="post-title"><?php the_title(); ?></h2>
-		<p class="author">by <?php the_author(); ?></p>
-		<p class="date"><?php the_date(); ?></p>
-
 		<hr class="title-break">
-
 		<p class="the-content"><?php the_content(); ?></p>
-
 		<div class="single-navigation">
 			<?php previous_post_link('<p>Prev: %link</p>'); ?>
 			<p><a href="<?php bloginfo('url'); ?>/work">Back to Work</a></p>
