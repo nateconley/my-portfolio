91486e8bb821e049ee41cbc0797aae87d8988ab1
Nathan Conley <nateconley123@gmail.com>
2016-05-05 12:08:46 -0700

    Added beatles project

---
+++ single-beatles.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<html>
+<head>
+	<title>Nate Conley | Beatles Quotes</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://necolas.github.io/normalize.css/3.0.2/normalize.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+	<link href='https://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+
+	<link rel="stylesheet" href="/portfolio-wp/wp-content/uploads/beatles.css">
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+
+	<script type="text/javascript" src="/portfolio-wp/wp-content/uploads/beatles.js"></script>
+</head>
+<body>
+	
+<div class="overlay"></div>
+<div id="quote-text"></div>
+<div id="wrapper">
+	<header>
+	  <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b0/Beatles_logo.svg/1000px-Beatles_logo.svg.png" alt="The Beatles">
+	  <h1>QUOTE GENERATOR</h1>
+	  <h2>by Nate Conley <span><a href="http://www.github.com/nateconley" target="_blank"><i class="fa fa-github fa-2x"></i></a></span></h2>
+	</header>
+
+	<main>
+	  <div id="images">
+	    <button id="paul" data-member="Paul McCartney"></button>
+	    <button id="john" data-member="John Lennon"></button>
+	    <button id="george" data-member="George Harrison"></button>
+	    <button id="ringo" data-member="Ringo Starr"></button>
+	  </div>
+	</main>
+	<div id="link">
+		<h3>Return to <a href="http://nateconley.com/portfolio/quote-generator/">nateconley.com</a></h3>
+	</div>
+</div>
+</body>
+</html>
